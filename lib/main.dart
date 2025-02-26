0808c38c4a7bf2440b57f0921ee8c03274534509
DANIEL_URIBE_MARTINEZ <a.22308051281323@cbtis128.edu.mx>
2025-02-26 20:08:52 +0000

    mandar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Daniel Uribe"),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Daniel Uribe Martinez Mat:22308051281323",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                SizedBox(height: 100), //esto realmente no se necesita.
+                Icon(
+                  Icons.sports_basketball,
+                  color: Colors.pink,
+                  size: 30.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                Icon(
+                  Icons.sports_esports,
+                  color: Colors.green,
+                  size: 30.0,
+                ),
+                Icon(
+                  Icons.sports_baseball,
+                  color: Colors.blue,
+                  size: 36.0,
+                ),
+                Icon(
+                  Icons.sports_football,
+                  color: Color(0xffc6bd40),
+                  size: 36.0,
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
-  }
-}
+  } // fin widgets
+} // fin clase
